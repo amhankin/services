29b8edf76b0788b03a160086328a5ebe08a8c485
Aaron Hankin <amhankin@gmail.com>
2016-04-03 13:31:30 -0600

    Added macports updated script. Added LaunchDaemon to automatically update macports installs on a weekly basis

--- macports/run_update.sh
+++ macports/run_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #set -e
 TODAY=`date '+%Y%m%d_%H%M%S'`
-LOGPATH='/srv/launchd/logs'
-SCRIPTPATH='/srv/launchd'
+LOGPATH='/srv/macports/logs'
+SCRIPTPATH='/srv/macports'
 SCRIPTNAME='update.sh'
 #echo $datetoday
 exec > >(tee -i ${LOGPATH}/backup_${TODAY}.log)
